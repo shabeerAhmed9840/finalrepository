7ca2cc7a7ab5af57de79cebe487b63ba207e5633
Aadhithyan <aadhithyanaadhi@gmail.com>
2019-12-12 11:49:19 +0530

    Update 1

--- src/main/java/final1/A.java
+++ src/main/java/final1/A.java
@@ -4,6 +4,12 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("hi world");
 		System.out.println("hellow world this in india");
+		int k=0;
+		for (int i = 0; i < 5; i++) {
+			System.out.println(k);
+		k++;
+		}
+		System.out.println("Aadhi added the for loop");
 	}
 
 }
