357602aad494cbd140da7c28a042994f87138969
shabeerahmed9840 <shabeerahmed1996@outlook.com>
2019-12-12 11:42:50 +0530

    thursday

--- src/main/java/final1/A.java
+++ src/main/java/final1/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("hi world");
 		System.out.println("hellow world this in india");
+		System.out.println("in india");
 	}
 
 }
