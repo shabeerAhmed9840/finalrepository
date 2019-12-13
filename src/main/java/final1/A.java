e055f149bcdd6064626c23ff6c4ee59ef71010bd
shabeerAhmed9840 <58761894+shabeerAhmed9840@users.noreply.github.com>
2019-12-13 08:45:22 +0530

    Error due to not saving in Eclipse - corrected
    forgot to save in eclipse before pushing

--- src/main/java/final1/A.java
+++ src/main/java/final1/A.java
@@ -4,16 +4,12 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("hi world");
 		System.out.println("hellow world this in india");
-<<<<<<< HEAD
 		int k=0;
 		for (int i = 0; i < 5; i++) {
 			System.out.println(k);
 		k++;
 		}
 		System.out.println("Aadhi added the for loop");
-=======
 		System.out.println("in india");
->>>>>>> b3253dcaf509c675104c1ed7de84cd2837a9914f
 	}
-
 }
